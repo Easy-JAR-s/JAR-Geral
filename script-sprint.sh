689101a1a88bd003bd5952e928327992721907e5
didiegovieira <diego.vieira@sptech.school>
2023-04-24 19:00:28 -0300

    script teste

--- script-sprint.sh
+++ script-sprint.sh
@@ -7,7 +7,19 @@ java -version
 if [ $? = 0 ]; #se retorno for igual a 0
 	then #entao,
 		echo "java instalado" #print no terminal
+		
+		if [ -d "Easy---JAR" ]
+		then
+			cd Easy---JAR
+			git fetch && git pull
+			cd Easy---JAR/project-easy/target
+		else
+			git clone https://github.com/PauloCezaretto/Easy---JAR.git
+			cd Easy---JAR/project-easy/target
+		fi
 
+	java -jar project-easy-1.0-SNAPSHOT-jar-with-dependencies.jar
+	
 	else #se nao,
 		echo "java não instalado" #print no terminal
 		echo "gostaria de instalar o java? [s/n]" #print no terminal
@@ -29,6 +41,7 @@ if [ $? = 0 ]; #se retorno for igual a 0
 		fi
 
 	java -jar project-easy-1.0-SNAPSHOT-jar-with-dependencies.jar
+	
 	fi #fecha o 2o if
 
 fi #fecha o 1o if
